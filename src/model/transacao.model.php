ff2d46a64d7720f40c239f76c0bd2a787ade08fa
Pedro Marinho <pedrohsmarinho@outlook.com>
2021-11-18 18:52:05 -0300

    alterando a model de transacao, na criaçao e fazendo a busca da transao #12 #13

--- src/model/transacao.model.php
+++ src/model/transacao.model.php
@@ -3,12 +3,27 @@ require '../../database/conectar.php';
 
 function criarTransacao($dados)
 {
-  var_dump($dados);
+  // var_dump($dados);
   try {
     global $pdo;
-    $query = $pdo->prepare('INSERT INTO TRANSACOES (ID_CARTEIRA, TIPO_TRANSA, DATA_TRANSA, VALOR_TRANSA) VALUES (:ID_CARTEIRA, :TIPO_TRANSA, :DATA_TRANSA, :VALOR_TRANSA)');
+    $query = $pdo->prepare('INSERT INTO TRANSACOES (ID_USUARIO, TIPO_TRANSA, DATA_TRANSA, VALOR_TRANSA, INFO)
+     VALUES (:ID_USUARIO,:TIPO_TRANSA, :DATA_TRANSA, :VALOR_TRANSA, :INFO)');
     $query->execute($dados);
   } catch (PDOException $e) {
     echo $e->getMessage();
   }
 };
+
+function buscarTransacao($user)
+{
+try{
+    global $pdo;
+    $query = $pdo->prepare("SELECT ID, TIPO_TRANSA, DATA_TRANSA, VALOR_TRANSA, INFO FROM TRANSACOES 
+    WHERE ID_USUARIO = $user");
+    $query->execute();
+    return $query->fetchAll();
+}
+catch(Exception $e){
+  echo $e->getMessage();
+}
+}
