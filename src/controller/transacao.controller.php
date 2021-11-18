efe20d4c9426f5b3144f41ebd2d0ac7c18cc19b8
Pedro Marinho <pedrohsmarinho@outlook.com>
2021-11-18 18:49:06 -0300

    GH-12 alteração do controller de Transação

--- src/controller/transacao.controller.php
+++ src/controller/transacao.controller.php
@@ -8,18 +8,29 @@ function Transacao()
   session_start();
 
   $today = date("Y-m-d");
-  // $carteira = buscarCarteira($_SESSION['usuario']['id']);
-
+  //$carteira = buscarCarteira($_SESSION['usuario']['id']);
+  $usuario = $_SESSION['usuario']['id'];
   $dados = [
-    'ID_CARTEIRA' => '1',
+    'ID_USUARIO' => $usuario,
     'TIPO_TRANSA' => $_POST['tipo'],
     'DATA_TRANSA' => $today,
-    'VALOR_TRANSA' => $_POST['valor']
+    'VALOR_TRANSA' => $_POST['valor'],
+    'INFO' => $_POST['info']
   ];
-
   // var_dump($dados);
   criarTransacao($dados);
-  // header("Location: ../view/home.php");
+  header("Location: ../view/home.php");
+}
+
+function transa()
+{
+  session_start();
+
+  $usuario = $_SESSION['usuario']['id'];
+
+  $dados = buscarTransacao($usuario);
+
+  return $dados;
 }
 
 $metodo = $_SERVER['REQUEST_METHOD'];
@@ -29,6 +40,14 @@ if ($metodo === 'POST') {
     Transacao();
   } catch (Exception $e) {
     header("location: ../view/cadastro.php?msg=" . $e->getMessage());
-    var_dump($e->getMessage());
+    // var_dump($e->getMessage());
   }
 }
+
+// if ($metodo === 'GET') {
+//   try {
+//     transa()
+//   } catch (Exception $e) {
+//     //throw $th;
+//   }
+// }
