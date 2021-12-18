6613222826de106c7ecbaf9ad2753b8c382838de
Pedro Marinho <pedrohsmarinho@outlook.com>
2021-12-18 00:51:40 -0300

    #14 correção de quebra de código

--- src/controller/update.controller.php
+++ src/controller/update.controller.php
@@ -4,29 +4,16 @@ require_once '../model/transacao.model.php';
 require_once '../model/carteira.model.php';
 require_once "../controller/redirect.controller.php";
 
-error_log("entrou na mulesta");
-error_log('valor que vem da função externa');
 if ($_GET['id_transa'] != null || $_GET['id_transa'] != "") {
+    
     function transa()
     {
 
         $id_transacao = $_GET['id_transa'];
-        error_log($id_transacao);
-        error_log('***');
-
-        error_log('ENTROU NESTA MERDA');
         session_start();
 
         $usuario = $_SESSION['usuario']['id'];
-        //$id_transacao = $_POST['id_transacao'];
         $dados = buscarTransacaoEdit($usuario, $id_transacao);
-        //var_dump($dados);
-
-        //var_dump($dados);
-        error_log('valor do id dentro do controller');
-        error_log($id_transacao);
-        error_log("valor da query");
-
         return $dados;
         buscarTransacaoEdit($dados, $id_transacao);
         header("Location: ../view/update.php");
@@ -54,7 +41,7 @@ if ($_GET['id_transa'] == null || $_GET['id_transa'] == "") {
         ];
 
         $msg = "Transação atualizada com sucesso";
-        // var_dump($dados);
+       // var_dump($dados);
         updateTransacao($dados);
         header("Location: ../view/home.php?msgsuccess=$msg");
     }
