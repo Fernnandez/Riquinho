3fff7806a6b342dfac07db8e41b33edfb176780b
Pedro Marinho <pedrohsmarinho@outlook.com>
2021-12-18 00:48:49 -0300

    #14 adição do redirecet de receita

---
+++ src/controller/redirect_receita.php
@@ -0,0 +1,6 @@
+<?php
+
+if ($_POST['id_transacao'] != null || $_POST['id_transacao'] != "") {
+    $id_update = $_POST['id_transacao'];
+    header("Location: ../view/update.receita.php?id_transa=$id_update");
+}
