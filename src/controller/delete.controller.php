b27302b2e2a71c69f174d76c4e7b3dc2dfc2c83b
Pedro Marinho <pedrohsmarinho@outlook.com>
2021-12-20 20:02:00 -0300

    #28 adiciontando mensagem

--- src/controller/delete.controller.php
+++ src/controller/delete.controller.php
@@ -6,10 +6,11 @@ $id = $_GET['id'];
 
 try {
   global $pdo;
+  $msg = "Transação deletada com sucesso";
   $query = $pdo->prepare("DELETE FROM TRANSACOES WHERE ID = ?");
   $query->bindParam(1, $id);
   $del = $query->execute();
-  header("Location: ../view/home.php");
+  header("Location: ../view/home.php?msgsuccess=$msg");
 } catch (Exception $e) {
   echo $e->getMessage();
 }
