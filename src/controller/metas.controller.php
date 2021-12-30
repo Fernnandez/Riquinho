a192d8a1ce500350758af4110ff3320f1c68cf97
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-12-30 01:57:57 -0300

    #34 criação de validação nos dados recebidos e identação de controller e model de metas

--- src/controller/metas.controller.php
+++ src/controller/metas.controller.php
@@ -1,47 +1,40 @@
 <?php
-
 require_once '../model/metas.model.php';
 
+function handleMetas()
+{
+    $msg = "Meta cadastrada com sucesso";
+    $msgError = "Algo deu errado";
+    $today = date("Y-m-d");
 
-    function handleMetas(){
-        error_log("entrou");
+    session_start();
 
-        $msg = "Meta cadastrada com sucesso";
-        $msgError = "Algo deu errado";
-        
-        session_start();
-      
-        $today = date("Y-m-d");
-
+    if ($_POST['urgencia'] != null && $_POST['data'] != null && $_POST['description'] != null && $_POST['value'] != null) {
         $dados = [
-          'ID_USUARIO' => $_SESSION['usuario']['id'],
-          'NIVEL_META' => $_POST['urgencia'],
-          'DATA_META' => isset($_POST['data']) ? $_POST['data'] : $today,
-          'DESCRICAO_META' => $_POST['description'],
-          'VALOR_META' => $_POST['value'],
+            'ID_USUARIO' => $_SESSION['usuario']['id'],
+            'NIVEL_META' => $_POST['urgencia'],
+            'DATA_META' => isset($_POST['data']) ? $_POST['data'] : $today,
+            'DESCRICAO_META' => $_POST['description'],
+            'VALOR_META' => $_POST['value'],
         ];
-
-        //var_dump($dados);
-
-        try {
-
-          createMetas($dados);
-          header("Location: ../view/metas.php?msgsuccess=$msg");
-          
-        } catch (\Throwable $th) {
-          header("Location: ../view/metas.php?errormsg=$msgError");
-        }
     }
 
-    $metodo = $_SERVER['REQUEST_METHOD'];
+    try {
+        createMetas($dados);
+        header("Location: ../view/metas.php?msgsuccess=$msg");
 
-    if ($metodo === 'POST') {
-        error_log("entrou no if");
-        try {
-            handleMetas();
-        } catch (Exception $e) {
-          //header("location: ../view/cadastro.php?msg=" . $e->getMessage());
-        }
-      }
-      
-?>
+    } catch (\Throwable $th) {
+        header("Location: ../view/metas.php?errormsg=$msgError");
+    }
+}
+
+//verificação do metodo de envio de dados
+$metodo = $_SERVER['REQUEST_METHOD'];
+if ($metodo === 'POST') {
+    error_log("entrou no if");
+    try {
+        handleMetas();
+    } catch (Exception $e) {
+        //header("location: ../view/cadastro.php?msg=" . $e->getMessage());
+    }
+}
--- src/model/metas.model.php
+++ src/model/metas.model.php
@@ -1,21 +1,22 @@
 <?php
 require '../../database/conectar.php';
 
-function createMetas($dados){
+function createMetas($dados)
+{
     session_start();
     $msgError = "Usuario Invalido/não autenticado";
     $user_identification = $_SESSION['usuario']['id'];
 
-    if($user_identification){
+    if ($user_identification) {
         try {
             global $pdo;
             $query = $pdo->prepare("INSERT INTO METAS (ID_USUARIO,NIVEL_META,DATA_META,DESCRICAO_META,VALOR_META)
              VALUES (:ID_USUARIO, :NIVEL_META, :DATA_META, :DESCRICAO_META, :VALOR_META)");
             $query->execute($dados);
-          } catch (PDOException $e) {
+        } catch (PDOException $e) {
             echo $e->getMessage();
-          }
-    }else{
+        }
+    } else {
         error_log("usuario não está logado");
         header("Location: ../view/metas.php?errormsg=$msgError");
     }
