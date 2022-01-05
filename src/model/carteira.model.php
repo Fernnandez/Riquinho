3d4552cb95554894497597c6fbaeaa2f0459a034
Pedro Marinho <pedrohsmarinho@outlook.com>
2022-01-05 11:08:13 -0300

    #31 model de carteira

--- src/model/carteira.model.php
+++ src/model/carteira.model.php
@@ -3,7 +3,7 @@ require '../../database/conectar.php';
 
 function criarCarteira($dados)
 {
-  var_dump($dados);
+  // var_dump($dados);
   try {
     global $pdo;
     $query = $pdo->prepare('INSERT INTO CARTEIRA (NOME, ID_USUARIO, DESCRICAO) VALUES 
@@ -23,6 +23,34 @@ function buscarCarteira($usuarioId)
     $sql = $pdo->prepare("SELECT ID, NOME, DESCRICAO FROM CARTEIRA WHERE ID_USUARIO = $usuarioId");
     $sql->execute();
     return $sql->fetchAll();
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+  }
+};
+
+function buscarCarteiraEdit($user, $id)
+{
+  try {
+    global $pdo;
+    $sql = $pdo->prepare("SELECT ID, ID_USUARIO, NOME, DESCRICAO 
+    FROM CARTEIRA WHERE ID_USUARIO = $user AND ID = $id");
+    $sql->execute();
+    return $sql->fetchAll();
+
+  } catch (Exception $e) {
+    echo $e->getMessage();
+  }
+}
+function updateCarteira($dados)
+{
+  // var_dump($dados);
+  try {
+    global $pdo;
+
+    $smt = $pdo->prepare('UPDATE CARTEIRA SET NOME = :NOME, DESCRICAO = :DESCRICAO, ID_USUARIO = :ID_USUARIO 
+    WHERE ID = ID');
+    $smt->execute($dados);
+    
   } catch (PDOException $e) {
     echo $e->getMessage();
   }
