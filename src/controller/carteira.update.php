f353ee4999a72989574c0d2aa4e6ff1892f3c9af
Pedro Marinho <pedrohsmarinho@outlook.com>
2022-01-05 11:08:47 -0300

    #31 controller carteira update

---
+++ src/controller/carteira.update.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once '../model/carteira.model.php';
+
+
+function carteira()
+
+{
+    $id = $_GET['idcarteira'];
+ 
+    $user = $_SESSION['usuario']['id'];
+    $dados = buscarCarteiraEdit($user, $id);
+    return $dados;
+    buscarCarteiraEdit($user, $id);
+    header("Location: ../view/carteira.update.php");
+    return carteira();
+}
+
+$metodo = $_SERVER['REQUEST_METHOD'];
+
+function update()
+{
+    session_start();
+
+    $dados = [
+        
+        'ID_USUARIO' => $_SESSION['usuario']['id'],
+        'NOME' => $_POST['name'],
+        'DESCRICAO' => $_POST['descricao'],
+    ];
+
+   var_dump($dados);
+     updateCarteira($dados);
+    }
+    
+    if ($metodo === 'POST') {
+        try {
+            $msg = "Carteira atualizada com sucesso";
+            update();
+      header("Location: ../view/carteira.php?msgsuccess=$msg");
+  } catch (Exception $e) {
+    var_dump($e->getMessage());
+  }
+}
